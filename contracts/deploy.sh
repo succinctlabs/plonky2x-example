3db501dc30df278608321d1e5359d95f0fbc1712
0x0 <coollightning123@gmail.com>
2023-08-28 14:16:38 -0700

    nit

--- contracts/deploy.sh
+++ contracts/deploy.sh
@@ -1,2 +1,2 @@
 source ../.env
-forge create src/Counter.sol:SimpleCircuit --rpc-url $RPC_URL --private-key $PRIVATE_KEY --verify --etherscan-api-key $ETHERSCAN_API_KEY
+forge create src/Counter.sol:SimpleCircuit --rpc-url $RPC_1 --private-key $PRIVATE_KEY --verify --etherscan-api-key $ETHERSCAN_API_KEY
